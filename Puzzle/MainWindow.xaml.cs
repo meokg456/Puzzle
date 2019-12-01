d9d72d4cdc74ca8fb55d0afe61eb1e5c3b28239c
DŨNG BÙI TIẾN <1712365@student.hcmus.edu.vn>
2019-12-01 17:44:59 +0700

    thêm cắt hình nhưng code bị lỗi chưa biết fix sao

--- Puzzle/MainWindow.xaml.cs
+++ Puzzle/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,62 @@ namespace Puzzle
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        const int startX = 30;
+        const int startY = 30;
+        const int width = 50;
+        const int height = 50;
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var screen=new OpenFileDialog();
+
+            if(screen.ShowDialog() == true)
+            {
+                var source = new BitmapImage(
+                    new Uri(screen.FileName, UriKind.Absolute));
+                Debug.WriteLine($"{source.Width} - {source.Height}");
+                previewImage.Width = 300;
+                previewImage.Height = 240;
+                previewImage.Source = source;
+                //bug.WriteLine();
+
+                Canvas.SetLeft(previewImage, 200);
+                Canvas.SetTop(previewImage, 20);
+
+                // Bat dau cat thanh 9 manh
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (!((i == 2) && (j == 2)))
+                        {
+                            var h = (int)source.Height;
+                            //var w = (int)source.Height;
+                            var w = (int)source.Width;
+                            //Debug.WriteLine($"Len = {len}");
+                            var rect = new Int32Rect(j * w, i * h, w, h);
+                            var cropBitmap = new CroppedBitmap(source,
+                                rect);
+
+                            var cropImage = new Image();
+                            cropImage.Stretch = Stretch.Fill;
+                            cropImage.Width = width;
+                            cropImage.Height = height;
+                            cropImage.Source = cropBitmap;
+                            canvas.Children.Add(cropImage);
+                            Canvas.SetLeft(cropImage, startX + j * (width + 2));
+                            Canvas.SetTop(cropImage, startY + i * (height + 2));
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
